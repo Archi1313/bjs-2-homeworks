8df009c36014e46ab1f31f91b2b469eda33dd064
Archi1313 <13archi@mail.ru>
2023-07-08 23:07:28 +0300

    3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+let cache = [];
+
+function wrapper(...args) {
+    const hash = md5(args); 
+    let objectInCache = cache.find((item) => item.hash===hash);
+    if (objectInCache) { 
+        console.log("Из кэша: " + objectInCache.value );
+        return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache[length]={hash: hash, value: result};
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId;
+
+  function wrapper(...args){
+    wrapper.allCount++; 
+    if (!timeoutId){
+      func(...args);
+      wrapper.count++;
+      } 
+
+    clearTimeout(timeoutId);  
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+      }, delay);
+  }
+
+  wrapper.count=0;
+  wrapper.allCount=0;
+
+  return wrapper;
 }
